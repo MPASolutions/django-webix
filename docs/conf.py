6c12e78205c8b9265a02cf15817bc9bf78db2981
Alessio Bazzanella <alessio.bazzanella@me.com>
2020-05-28 17:52:00 +0200

    Fix sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -23,8 +23,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tests.settings')
 # sys.path.insert(0, os.path.abspath('.'))
 
 sys.path.insert(0, os.path.abspath('..'))
-import django
-django.setup()
 
 import django_webix
 
@@ -35,7 +33,7 @@ import django_webix
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'settings']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
