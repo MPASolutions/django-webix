bf0a6e9df0634267d2f257aad7f222403e65e3fb
Alessio Bazzanella <bazzanella@mpasol.it>
2023-02-15 12:30:18 +0100

    fix urls and extends

--- django_webix/contrib/auth/templates/django_webix/auth/password_reset/base.js
+++ django_webix/contrib/auth/templates/django_webix/auth/password_reset/base.js
@@ -1,11 +1,11 @@
-{% extends 'auth_webix/base_no_auth.html' %}
+{% extends 'django_webix/auth/base_no_auth.html' %}
 
 {% block webix_content %}
     webix.ui([], $$("{{ webix_container_id }}"));
 
     {% if 'uid' in request.GET and 'token' in request.GET %}
-        load_js("{% url 'dwauth:password_reset.confirm' uidb64=request.GET.uid token=request.GET.token %}");
+        load_js("{% url 'dwauth.password_reset.confirm' uidb64=request.GET.uid token=request.GET.token %}");
     {% else %}
-        load_js("{% url "dwauth:password_reset.base" %}");
+        load_js("{% url "dwauth.password_reset.form" %}");
     {% endif %}
 {% endblock %}
