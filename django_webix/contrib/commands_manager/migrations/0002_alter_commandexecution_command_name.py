85e5220d4346e3dbe85abfa1dc71b0b4c07dd098
Alessandro Regolini <regolini@mpasol.it>
2023-02-17 10:21:26 +0000

    Clean migrations commands manager2

---
+++ django_webix/contrib/commands_manager/migrations/0002_alter_commandexecution_command_name.py
@@ -0,0 +1,12 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('dwcommands_manager', '0001_initial'),
+    ]
+
+    operations = [
+    ]
+
