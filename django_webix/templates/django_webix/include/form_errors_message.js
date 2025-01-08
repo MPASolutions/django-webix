93937036786a4f09e29b1a71328d05ccc35153ce
Alessandro Regolini <alessandro.regolini@enogis.it>
2025-01-08 07:31:18 +0000

    fix-action-message-pre-form-load

--- django_webix/templates/django_webix/include/form_errors_message.js
+++ django_webix/templates/django_webix/include/form_errors_message.js
@@ -1,21 +1,28 @@
 function show_errors(errors) {
     var li_text_errors = '';
-    errors.forEach(function (item, index) {
-        if (item.label!=null) {
-            li_text_errors += "<li><b>" + item.label + "</b>: " + item.error + "</li>"
+    // check if is an array of strings or array of dicts
+    if (errors.length > 0){
+        if (typeof errors[0] === "string") {
+            errors.forEach(function (txt_error) {
+                li_text_errors += "<li><b>" + txt_error + "</b>"
+            });
         } else {
-            li_text_errors += "<li>" + item.error + "</li>"
+            errors.forEach(function (item, index) {
+                if (item.label!=null) {
+                    li_text_errors += "<li><b>" + item.label + "</b>: " + item.error + "</li>"
+                } else {
+                    li_text_errors += "<li>" + item.error + "</li>"
+                }
+            });
         }
-        });
-    if (errors.length > 0) {
         webix.message({
             type: "error",
             text: "{{_("Oops! Something went wrong...")|escapejs}}" +
-                "<br><br>" +
-                "{{_("Error in the following fields:")|escapejs}}" +
-                "<ul>" +
-                li_text_errors +
-                "</ul>"
+                  "<br><br>" +
+                  "{{_("Error in the following fields:")|escapejs}}" +
+                  "<ul>" +
+                  li_text_errors +
+                  "</ul>"
         });
     }
 }
