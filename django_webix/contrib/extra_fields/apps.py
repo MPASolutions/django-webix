3c36dbc9228fafec868e895a627ef16c70cdd096
Alessandro Regolini <alessandro.regolini@enogis.it>
2025-04-24 11:12:31 +0200

    fix-migrations-dwextrafields-initial

--- django_webix/contrib/extra_fields/apps.py
+++ django_webix/contrib/extra_fields/apps.py
@@ -1,3 +1,5 @@
+import sys
+
 from django.apps import AppConfig
 from django.db import connection, models
 from django.utils.translation import gettext_lazy as _
@@ -11,20 +13,21 @@ class ExtraFieldsConfig(AppConfig):
     verbose_name = _("Django Webix Extra Fields")
 
     def ready(self):
-        if "dwextra_fields_modelfield" in connection.introspection.table_names():
-            from django_webix.contrib.extra_fields.models import ModelField
+        if "makemigrations" not in sys.argv and not any("migrate" in arg for arg in sys.argv):
+            if "dwextra_fields_modelfield" in connection.introspection.table_names():
+                from django_webix.contrib.extra_fields.models import ModelField
 
-            for mf in ModelField.objects.all():
-                _model = mf.content_type.model_class()
-                field_class = getattr(models, mf.field_type)
+                for mf in ModelField.objects.all():
+                    _model = mf.content_type.model_class()
+                    field_class = getattr(models, mf.field_type)
 
-                class CustomNotDbColumnField(NotDbColumnField, field_class):
-                    pass
+                    class CustomNotDbColumnField(NotDbColumnField, field_class):
+                        pass
 
-                if mf.field_type != "ForeignKey":
-                    _field = CustomNotDbColumnField(blank=True, null=True, verbose_name=mf.label)
-                else:
-                    related_to_class = mf.related_to.model_class()
-                    _field = CustomNotDbColumnField(related_to_class, blank=True, null=True, verbose_name=mf.label)
-                # private_only=True prevent migrations not required
-                _field.contribute_to_class(_model, mf.field_name, private_only=True)
+                    if mf.field_type != "ForeignKey":
+                        _field = CustomNotDbColumnField(blank=True, null=True, verbose_name=mf.label)
+                    else:
+                        related_to_class = mf.related_to.model_class()
+                        _field = CustomNotDbColumnField(related_to_class, blank=True, null=True, verbose_name=mf.label)
+                    # private_only=True prevent migrations not required
+                    _field.contribute_to_class(_model, mf.field_name, private_only=True)
