6166cdecc1fcede1d4a4ebaf6bcede3f3a03ddc5
Kristjan Gjika <gjika@mpasol.it>
2025-10-03 11:48:51 +0200

    fix full_clean of model with extra fields

--- django_webix/contrib/extra_fields/fields.py
+++ django_webix/contrib/extra_fields/fields.py
@@ -1,6 +1,10 @@
 class NotDbColumnField:
     db_collation = False
 
+    # skip Model.full_clean validation, the field is present in _meta.fields but
+    # is not present in the object instance.
+    generated = True
+
     def get_attname_column(self):
         attname = self.get_attname()
         return attname, None  # None for concrete=False -> not caming from DB
